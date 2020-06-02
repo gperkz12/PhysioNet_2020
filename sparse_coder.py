b82182d1abe6809296a9d572140970a58d6bf78d
Chase <jmcglinn18@gmail.com>
2020-06-02 09:54:31 -0700

    attempt on sparse coding

---
+++ sparse_coder.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+from sklearn.decomposition import dict_learning
+from sklearn import datasets
+
+#import iris practice data
+iris = datasets.load_iris()
+X = iris.data
+
+
+
+
+
+
+#use OMP to clean up the signal
+
