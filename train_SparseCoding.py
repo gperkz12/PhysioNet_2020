117b958db9b3df5e650d98596e227555318b46bb
Chase <jmcglinn18@gmail.com>
2020-06-03 15:45:11 -0700

    added indexing lines for testing and training

--- train_SparseCoding.py
+++ train_SparseCoding.py
@@ -7,18 +7,23 @@ from sklearn import datasets
 iris = datasets.load_iris()
 X = iris.data
 
+rnd_index = np.random.permutation(X.shape[0])
+num_test = round(X.shape[0] * 0.2)
+test_index = rnd_index[0:num_test]
+train_index = rnd_index[num_test:]
+
+
+X_train = X[train_index][:]
+X_test = X[test_index][:]
+
 #uses the dictionary learning class to transform the data
 
 atoms = DictionaryLearning(None, 1, 1000, 1e-08, 'lars', 'omp', None, None, None, None, None, False, False, None, False, False)
 
-#fit the data
-atoms.fit(X, None)
+#fit and transform data
 
-#fit the transform data
-atoms.fit_transform(X, None)
-
-#transform the data
-xNew = atoms.transform(X)
+sparsedata = atoms.fit_transform(X_test, None)
 
 #print the new sparse data
-print(xNew)
+print(X.shape, sparsedata.shape)
+print(sparsedata)
