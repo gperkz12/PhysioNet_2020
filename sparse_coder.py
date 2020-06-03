faa08edfff2af15ac52ff8a2c20ef840a67a8a21
Chase <jmcglinn18@gmail.com>
2020-06-03 12:03:54 -0700

    continued working on sparse coding

--- sparse_coder.py
+++ sparse_coder.py
@@ -1,22 +1,33 @@
 
 import numpy as np
-from sklearn.decomposition import dict_learning
+from sklearn.decomposition import DictionaryLearning
 from sklearn import datasets
 
 #import iris practice data
 iris = datasets.load_iris()
 X = iris.data
 
-def transform(self, X):
-    n_samples_transform = X.shape[0]
+#uses the dictionatry learning function to transform the data
+#atoms = dict_learning(X, 4, 1, 100, 1e-8, 'lars', None, None, None, None, False, None, False, False, False)
 
-def transform(self, X):
-    return self._transform(X)
-
-def fit_transform(self, X, y=None):
-    return self.fit(X)._transform(X=None)
+atoms = DictionaryLearning(None, 1, 1000, 1e-08, 'lars', 'omp', None, None, None, None, None, False, False, None, False, False)
 
 
 
-#use OMP to clean up the signal
+
+
+#print the sparse data
+
+atoms.fit(X, None)
+
+atoms.fit_transform(X, None)
+
+params = atoms.get_params(True)
+
+atoms.set_params()
+
+xNew = atoms.transform(X)
+
+print(xNew)
+
 
