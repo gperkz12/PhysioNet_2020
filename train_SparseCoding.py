8abc97483b6e5555e8d43054ca272ad1e3e6e2de
Garrett Perkins <gperkz12@gmail.com>
2020-06-03 16:17:40 -0600

    Moved Code from sparse_coder to train_sparseCoding

---
+++ train_SparseCoding.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+from sklearn.decomposition import DictionaryLearning
+from sklearn import datasets
+
+#import iris practice data
+iris = datasets.load_iris()
+X = iris.data
+
+#uses the dictionary learning class to transform the data
+
+atoms = DictionaryLearning(None, 1, 1000, 1e-08, 'lars', 'omp', None, None, None, None, None, False, False, None, False, False)
+
+#fit the data
+atoms.fit(X, None)
+
+#fit the transform data
+atoms.fit_transform(X, None)
+
+#transform the data
+xNew = atoms.transform(X)
+
+#print the new sparse data
+print(xNew)
