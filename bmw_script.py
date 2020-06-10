1294b7d5a2a36d2701027d9b525d7fe2c3d30a5f
bwhitaker8 <64041973+bwhitaker8@users.noreply.github.com>
2020-06-10 17:37:59 -0600

    Minor update to bmw_script.py

--- bmw_script.py
+++ bmw_script.py
@@ -1,10 +1,10 @@
 import data_read
 
-[list_data,list_label] = data_read.data_files_load('./DATA2')
+[list_data,list_label] = data_read.data_files_load('./DATA/TrainData_FeatureExtraction')
 
-for n in list_data:
-    print(n.shape)
+#for n in list_data:
+#    print(n.shape)
 
 
-print('................')
-print(list_label)
+#print('................')
+print(len(list_label))
