a336fb3472e2ebebabcc295a78d0991c8c9b3c28
Chase <jmcglinn18@gmail.com>
2020-06-18 16:18:50 -0400

    added comments & made changes

--- get_label.py
+++ get_label.py
@@ -1,20 +1,31 @@
 import csv
 import os
 
+#FINDING THE PATH TO THE CSV FILE
 path = os.path.abspath("DATA\\REFERENCE.csv")
 
-def get_label(filestr):
+def getLabel(filestr):
 
-    filestr = filestr.replace("A"," ")
-    filestr = filestr.lstrip('0')
-    idx = int(filestr)
+#FINDING THE INDEX
+    filestr = filestr.replace("A"," ")             #takes the input and removes the A char
+    filestr = filestr.lstrip('0')                  #strips leading zeros
+    idx = int(filestr)                             #converts filestr to a usable index and then is stored in idx
+
+#READING THE FILE
+    with open(path,'r') as csv_file:                #opens the file
+        csv_reader = list(csv.reader(csv_file))     #reads the csv and then puts it in a list
+        label = csv_reader[idx][1:]                 #the output is the line from the list indexed by the input
+        return(label)
 
 
-    with open(path,'r') as csv_file:
-        csv_reader = list(csv.reader(csv_file))
+#CODE TO TEST THE FUNCTION
+"""test1 = getLabel("A0001")
+print(test1)
 
-        print(csv_reader[idx][1:])
+test2 = getLabel("A0043")
+print(test2)"""
 
 
 
-get_label("A1002")
+
+
