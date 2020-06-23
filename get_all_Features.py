c68d9f12256a718e488c462abaae6c269e7741b7
Garrett Perkins <gperkz12@gmail.com>
2020-06-23 09:43:00 -0600

    Working

--- get_all_Features.py
+++ get_all_Features.py
@@ -1,8 +1,11 @@
 import numpy as np
 import scipy.io as sio
 import pickle as pk
+import get_12ECG_features
+import data_read
 from scipy.signal import butter, lfilter
 from scipy import stats
+
 
 # For pickling
 def save_object(obj, filename):
@@ -11,20 +14,23 @@ def save_object(obj, filename):
 
 # data files load
 
-<<<<<<< HEAD
-def get_all_features
-    read_dir(traindata)
-    for all_filenames
+def get_all_features(data, header_data):
+    i = 0
+    traindata = 'DATA\TrainData_Classifier'
+    data_read.data_files_list(traindata)
+    for all_filenames in traindata:
         curfile = all_filenames(i)
-        [data, headerdata] = load(curfile)
-        F[i] = get_all_features(data, headerdata)
+        [data, header_data] = data_read.data_files_load(curfile)
+        F[i] = get_file_features(data, header_data)
+        data_read(traindata, header_data)
+        pca_data[i] = F[i]
+    save_object(pca_data, pca_data.pkl)
+
+
 
         
-def get_12ECG_features(data, header_data):
-=======
 
-def get_all_features(data, header_data):
->>>>>>> master
+def get_file_features(data, header_data):
     tmp_hea = header_data[0].split(' ')
     ptID = tmp_hea[0]
     num_leads = int(tmp_hea[1])
@@ -36,7 +42,7 @@ def get_all_features(data, header_data):
         gain_lead[ii] = int(tmp_hea[2].split('/')[0])
 
     #   We are only using data from lead1
-    peaks, idx = detect_peaks(data[0], sample_Fs, gain_lead[0])
+    peaks, idx = get_12ECG_features.detect_peaks(data[0], sample_Fs, gain_lead[0])
 
     #   mean
     mean_RR = np.mean(idx / sample_Fs * 1000)
@@ -63,10 +69,3 @@ def get_all_features(data, header_data):
     kurt_Peaks = stats.kurtosis(peaks * gain_lead[0])
 
 # Need to pickle the features here
-
-def get_all_features():
-
-
-
-
-    save_object(features, "features.pkl")
