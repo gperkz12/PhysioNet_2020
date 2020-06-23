b85bbff774d436043d554e1a1a15cd3dfa74f1ab
bwhitaker8 <64041973+bwhitaker8@users.noreply.github.com>
2020-06-23 12:28:51 -0600

    Update bmw_script.py

--- bmw_script.py
+++ bmw_script.py
@@ -1,10 +1,16 @@
-import data_read
+#import data_read
+import get_label
 
-[list_data,list_label] = data_read.data_files_load('./DATA/TrainData_FeatureExtraction')
+#[list_data,list_label] = data_read.data_files_load('./DATA/TrainData_FeatureExtraction')
 
 #for n in list_data:
 #    print(n.shape)
 
 
 #print('................')
-print(len(list_label))
+#print(len(list_label))
+
+test1 = get_label.getLabel("A0001")
+print(test1)
+test2 = get_label.getLabel("A1236")
+print(test2)
