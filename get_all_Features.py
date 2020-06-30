4eb1237dbce7e7519c521e70855c54613489866d
Garrett <gperkz12@gmail.com>
2020-06-30 11:33:30 -0600

    Auto stash before merge of "master" and "origin/master"

--- get_all_Features.py
+++ get_all_Features.py
@@ -6,7 +6,16 @@ import data_read
 from scipy.signal import butter, lfilter
 from scipy import stats
 
+<<<<<<< Updated upstream
 def get_all_features():
+=======
+# data files load
+
+def get_all_features(data, header_data):
+    print(header_data)
+    print(data)
+    pca_data = []
+>>>>>>> Stashed changes
     traindata = 'DATA\TrainData_FeatureExtraction'
     [data, header_data, BAD_LABELS] = data_read.data_files_load(traindata)
     for i in range(0, (len(data))):
@@ -24,6 +33,8 @@ def get_all_features():
 
 
 def get_file_features(data, header_data):
+    print(header_data)
+    print(data)
     tmp_hea = header_data[0].split(' ')
     ptID = tmp_hea[0]
     num_leads = int(tmp_hea[1])
@@ -33,37 +44,45 @@ def get_file_features(data, header_data):
     for ii in range(num_leads):
         tmp_hea = header_data[ii + 1].split(' ')
         gain_lead[ii] = int(tmp_hea[2].split('/')[0])
+    for i in data:
+        #   We are only using data from lead1
+        peaks, idx = get_12ECG_features.detect_peaks(data[i], sample_Fs, gain_lead[0])
 
-    #   We are only using data from lead1
-    peaks, idx = get_12ECG_features.detect_peaks(data[0], sample_Fs, gain_lead[0])
+        #   mean
+        mean_RR = np.mean(idx / sample_Fs * 1000)
+        mean_Peaks = np.mean(peaks * gain_lead[0])
 
-    #   mean
-    mean_RR = np.mean(idx / sample_Fs * 1000)
-    mean_Peaks = np.mean(peaks * gain_lead[0])
+        #   median
+        median_RR = 0
+        median_Peaks = np.median(peaks * gain_lead[0])
 
-    #   median
-    median_RR = 0
-    median_Peaks = np.median(peaks * gain_lead[0])
+        #   standard deviation
+        std_RR = np.std(idx / sample_Fs * 1000)
+        std_Peaks = np.std(peaks * gain_lead[0])
 
-    #   standard deviation
-    std_RR = np.std(idx / sample_Fs * 1000)
-    std_Peaks = np.std(peaks * gain_lead[0])
+        #   variance
+        var_RR = stats.tvar(idx / sample_Fs * 1000)
+        var_Peaks = stats.tvar(peaks * gain_lead[0])
 
-    #   variance
-    var_RR = stats.tvar(idx / sample_Fs * 1000)
-    var_Peaks = stats.tvar(peaks * gain_lead[0])
+        #   Skewness
+        skew_RR = stats.skew(idx / sample_Fs * 1000)
+        skew_Peaks = stats.skew(peaks * gain_lead[0])
 
-    #   Skewness
-    skew_RR = stats.skew(idx / sample_Fs * 1000)
-    skew_Peaks = stats.skew(peaks * gain_lead[0])
+        #   Kurtosis
+        kurt_RR = stats.kurtosis(idx / sample_Fs * 1000)
+        kurt_Peaks = stats.kurtosis(peaks * gain_lead[0])
 
-    #   Kurtosis
-    kurt_RR = stats.kurtosis(idx / sample_Fs * 1000)
-    kurt_Peaks = stats.kurtosis(peaks * gain_lead[0])
+        features = np.hstack([mean_RR,mean_Peaks,median_RR,median_Peaks,std_RR,std_Peaks,var_RR,var_Peaks,skew_RR,skew_Peaks,kurt_RR,kurt_Peaks])
 
+<<<<<<< Updated upstream
     features = np.hstack([mean_RR,mean_Peaks,median_RR,median_Peaks,std_RR,std_Peaks,var_RR,var_Peaks,skew_RR,skew_Peaks,kurt_RR,kurt_Peaks])
 
     return features
+=======
+        print(features)
+        ++i
+        return features
+>>>>>>> Stashed changes
 
 
 # For pickling
