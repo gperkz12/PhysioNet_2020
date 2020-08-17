c9c946b13206395acf35698cf648a1eac818bf30
Garrett <gperkz12@gmail.com>
2020-08-16 20:29:50 -0600

    Sparse

--- train_SparseCoding.py
+++ train_SparseCoding.py
@@ -4,29 +4,6 @@ from sklearn.decomposition import DictionaryLearning
 from sklearn import datasets
 from sklearn import preprocessing
 
-<<<<<<< HEAD
-#load fourier data
-fourier_data = pk.load(open("Fourier_data.pkl", 'rb'))
-X = fourier_data
-#test
-def save_object(obj, filename):
-    with open(filename, 'wb') as output:  # Overwrites any existing file.
-        pk.dump(obj, output, pk.HIGHEST_PROTOCOL)
-
-
-rnd_index = np.random.permutation(X.shape[0])
-num_test = round(X.shape[0] * 0.2)
-test_index = rnd_index[0:num_test]
-train_index = rnd_index[num_test:]
-
-
-X_train = X[train_index][:]
-X_test = X[test_index][:]
-
-#uses the dictionary learning class to transform the data
-
-atoms = DictionaryLearning(100, 5, 1000, 1e-8, 'lars', 'lasso_lars')
-=======
 #load pca data
 Fourier_data = pk.load(open("Fourier_data.pkl", 'rb'))
 X = Fourier_data
@@ -34,7 +11,6 @@ X = Fourier_data
 #uses the dictionary learning class to transform the data
 
 atoms = DictionaryLearning(100, 1, 1000, 1e-8, 'lars', 'lasso_lars')
->>>>>>> master
 
 #fit and transform data
 atoms.fit(X)
