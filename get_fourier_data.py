96bf4d9978a76fe352679eb63f65a90a10c362d8
Garrett <gperkz12@gmail.com>
2020-08-16 20:25:19 -0600

    Auto stash before merge of "master" and "origin/master"
    W

--- get_fourier_data.py
+++ get_fourier_data.py
@@ -14,35 +14,16 @@ from scipy import fft
 
 def get_all_features():
     # When on windows it's a backslash slash, on linux its a forward slash
-    traindata = 'DATA/TrainData_FeatureExtraction'
+    traindata = 'DATA/TrainData_Classifier'
     [data, labels, filenames, header_data] = data_read.data_files_load(traindata)
     for i in range(0, (len(data))):
-        curfeatures = get_fourier_data(data[i], header_data[i])
-        if i == 0:
-            Fourier_data = curfeatures
-        else:
-            tmp = np.column_stack((Fourier_data, curfeatures))
-            Fourier_data = tmp
-
+         curfeatures = get_fourier_data(data[i], header_data[i])
+         if i == 0:
+             Fourier_data = curfeatures
+         else:
+             tmp = np.vstack((Fourier_data, curfeatures))
+             Fourier_data = tmp
     print(Fourier_data.shape)
-
-    #plt.style.use('ggplot')
-    #plt.hist(Fourier_data, bins=20)
-    #plt.show()
-    n_samples = 8192
-    t0 = 0
-    t1 = 1
-
-    transform_data = np.fft.fft(Fourier_data, n_samples)
-    print(transform_data.shape)
-
-    first_4096 = transform_data[:, 0:round(n_samples/2)]
-
-    amplitudes = np.abs(transform_data)
-
-    plt.show()
-    print(first_4096.shape)
-    print(amplitudes)
 
     save_object(Fourier_data, 'Fourier_data.pkl')
 
@@ -61,13 +42,21 @@ def get_fourier_data(data, header_data):
     for ii in range(num_leads):
         gain_lead[ii] = header_data[3+ii]
 
+    n_samples = 8192
+    for i in range(0, len(data)):
+        curdata = np.fft.fft(data[i], n_samples)
+        if i == 0:
+            Fdata = curdata
+        else:
+            tmp = np.vstack((Fdata, curdata))
+            Fdata = tmp
 
-    tmp_fourier_data = len(data[0])
-    return tmp_fourier_data
+    Fdata = np.abs(Fdata)
 
+    Fdata = Fdata[:, 0:round(n_samples / 2)]
+    return Fdata
+test
 # For pickling
 def save_object(obj, filename):
     with open(filename, 'wb') as output:  # Overwrites any existing file.
         pk.dump(obj, output, pk.HIGHEST_PROTOCOL)
-
-# Try axcept
